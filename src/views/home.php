a6577d817735216771548a6b97ff207d672c885e
metinbaris <hizmetparki@gmail.com>
2020-09-23 13:03:27 +0300

    contact form condition added to order id & css changes

--- src/views/home.php
+++ src/views/home.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Form</title>
     <link href="/css/app.css" rel="stylesheet"/>
 </head>
@@ -9,24 +10,28 @@
 <h1 class="pt-40">Contact Form</h1>
 <div class="container">
     <form class="contactForm">
-        <div id="contact-details">
+        <div id="contact_details">
             <label for="customer_name">Name*</label>
-            <input type="text" name="customer_name" placeholder="Name">
+            <input type="text" name="customer_name" placeholder="Name" required>
             <label for="email_address">Email*</label>
-            <input type="email" name="email_address" placeholder="Email">
+            <input type="email" name="email_address" placeholder="Email" required>
             <label for="">Type of Enquiry*</label>
-            <select id="enquiry_type" onchange="toggleOrderNumber()">
+            <select id="enquiry_type" onchange="toggleOrderNumber()" required>
                 <option>General</option>
                 <option>Regarding An Order</option>
             </select>
             <div id="enquiry_type_dependent" class="hidden">
-                <label for="order_number">Order Id</label>
-                <input type="text" name="order_number" minlength="6" maxlength="6" placeholder="Example: 004389">
+                <label for="order_number">Order Id*</label>
+                <input type="text" id="order_number" name="order_number" minlength="6" maxlength="6" placeholder="E.g. 004389">
             </div>
         </div>
-        <div id="message-box">
-            <label for="message">Message</label>
-            <textarea name="message" id="message" maxlength="1000"></textarea>
+        <div id="message_box">
+            <label for="message">Message*</label>
+            <textarea name="message" id="message" maxlength="1000" required></textarea>
+        </div>
+        <div id="terms_and_conditions_box">
+            <input type="checkbox" id="terms_and_conditions" name="terms_and_conditions" required>
+            <label for="terms_and_conditions">I have read and i agree to the data protection policy.*</label>
         </div>
         <button>Submit</button>
     </form>
@@ -34,6 +39,7 @@
 <script type="text/javascript">
   function toggleOrderNumber () {
     document.getElementById('enquiry_type_dependent').classList.toggle('hidden')
+    document.getElementById('order_number').toggleAttribute('required')
   }
 </script>
 </body>
