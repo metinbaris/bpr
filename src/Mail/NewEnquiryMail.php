bf53ddb64095cff953d7d796ebd8ea2a52f183a3
metinbaris <hizmetparki@gmail.com>
2020-09-26 20:27:40 +0300

    SMTP debug is off

--- src/Mail/NewEnquiryMail.php
+++ src/Mail/NewEnquiryMail.php
@@ -50,7 +50,7 @@ class NewEnquiryMail
      */
     private function setMailServerSettings(PHPMailer $mail)
     {
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;// Enable verbose debug output
+        $mail->SMTPDebug = SMTP::DEBUG_OFF;// Enable verbose debug output
         $mail->isSMTP();// Send using SMTP
         $mail->Host = getenv('SMTP_HOSTNAME');// Set the SMTP server to send through
         $mail->SMTPAuth = true;// Enable SMTP authentication
